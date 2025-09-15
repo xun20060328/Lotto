061c7598644a99491f56c87fcf8901b424a29159
宋佳勳 <a168111bb@gmail.com>
2025-09-15 15:29:30 +0800

    第六次commit (狀態管理)

--- app/src/main/java/tw/edu/pu/csim/s1130305/lotto/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/csim/s1130305/lotto/MainActivity.kt
@@ -4,15 +4,22 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import tw.edu.pu.csim.s1130305.lotto.ui.theme.LottoTheme
-
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,8 +27,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             LottoTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    play(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -31,17 +37,23 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun play(modifier: Modifier = Modifier) {
+    var lucky by remember { mutableStateOf((1..100).random()) }
+    //var lucky = (1..100).random()
+    Column(modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+        Text(
+            text = "樂透數字(1-100)為 $lucky",
+        )
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    LottoTheme {
-        Greeting("Android")
+        Button(
+            onClick = { lucky = (1..100).random() }
+        ) {
+            Text("重新產生樂透碼")
+
+        }
     }
+
 }
